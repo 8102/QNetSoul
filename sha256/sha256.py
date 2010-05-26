f6ba321cde953ff7bdcf6563fe19f98b4b88de5b
LeFlou <leflou@tuxfamily.org>
2010-05-26 19:44:33 +0200

    Python checksum script added (sha256)

---
+++ sha256/sha256.py
@@ -0,0 +1,14 @@
+import os, sys
+from Crypto.Hash import SHA256
+
+def main():
+    if len(sys.argv) != 2:
+        print "usage: python", sys.argv[0], "<file>"
+        return
+
+    f = open(sys.argv[1]).read()
+    sha256 = SHA256.new(f)
+    print sha256.hexdigest()
+
+if __name__ == "__main__":
+    main()
